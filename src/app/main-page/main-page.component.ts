0d56dbd677ae22534df2421cb503a5e8694f158a
abd_a616 <abdalrhmanalherbawi@gmail.com>
2022-12-03 02:00:33 +0300

    update project and add slid cards

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,16 +1,24 @@
 import {Component, OnInit,} from '@angular/core';
 
+
+
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
-  styleUrls: ['./main-page.component.css']
+  styleUrls: ['./main-page.component.css'],
+  providers: []
 })
 export class MainPageComponent implements OnInit {
 
-  headsInTop :string[] = ["Top 10 Books","You May Like","Favorite 7 Books","Based On Similar User"];
-  imgPath = "https://books.google.com/books/publisher/content/images/frontcover/zlNdEAAAQBAJ?fife=w480-h690";
-  slides: any[] = new Array(3).fill({id: -1, src: '', title: '', subtitle: ''});
-  constructor() { }
-  ngOnInit(): void {}
+  imgPath= "https://books.google.com/books/publisher/content/images/frontcover/SSRGEAAAQBAJ?fife=w480-h690"
+  headsInTop: string[] = [ "You May Like", "Favorite 7 Books", "Based On Similar User"];
+  images = [1055, 194, 368, 36].map((n) => `https://picsum.photos/id/${n}/900/500`);
+  images2 = [123, 14, 38, 63].map((n) => `https://picsum.photos/id/${n}/900/500`);
+  images3 = [15, 134, 4, 434].map((n) => `https://picsum.photos/id/${n}/900/500`);
+  //allimg = [this.images, this.images2, this.images3];
+  constructor() {
+  }
+  ngOnInit(): void {
+  }
 
 }
