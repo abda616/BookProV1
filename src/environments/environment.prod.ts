8bf19b593b262c2eb9574f789d5e86d072973e2f
abd_a616 <abdalrhmanalherbawi@gmail.com>
2022-12-18 00:42:22 +0300

    fix server and add nodemon

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: 'http://176.29.106.103/',
-  registerUrl: 'http://localhost:3000/register',
-  userUrl: 'http://localhost:3000/users',
+  endPointUrl:'http://localhost:3000',
 };
