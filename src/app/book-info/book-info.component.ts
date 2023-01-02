8e019c6812430d2e5c7d12af00e911e49afa9ea6
abd_a616 <abdalrhmanalherbawi@gmail.com>
2023-01-02 23:03:21 +0300

    move data from mainPage to showingPage make class BookDemo.ts fix searchPage search

--- src/app/book-info/book-info.component.ts
+++ src/app/book-info/book-info.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
 import {BooInfoService} from '../services/boo-info.service';
+import {BookDataService} from "../services/Transfer/book-data.service";
 
 @Component({
   selector: 'app-my-book-info',
@@ -8,48 +9,27 @@ import {BooInfoService} from '../services/boo-info.service';
   styleUrls: ['./book-info.component.css']
 })
 export class MyBookInfoComponent implements OnInit {
-  currentBookInfo = {
-    title: "Atomic Habits",
-    author: "Stephenie Meyer",
-    ratings: "4.4",
-    imgSrc: "./assets/picture/atomic.jpg",
-    genres: [
-      "Self-Help"
-      ,
-      "non-fiction"
-      ,
-      "Drama"
-      ,
-      "Romance"
-    ],
-    plot: "No matter your goals, Atomic Habits offers a proven framework for improving--every day. James Clear, one of the world's leading experts on habit formation, reveals practical strategies that will teach you exactly how to form good habits, break bad ones, and master the tiny behaviors that lead to remarkable results. If you're having trouble changing your habits, the problem isn't you.  ",
-
-    language: "English",
-    numberOfPages: 350,
-    releaseDate: "13-sep-2017",
-    rating: 3,
-  }
-
+  currentBookInfo: any = '';
   similarAuthorBooks = []
-
   counter = 3;
   stars = [1, 2, 3, 4, 5]
-  booksDemo = [{
-    title: "Atomic Habits",
-    author: "James Clear",
-    imgSrc: "./assets/picture/atomic.jpg",
-    rating: "4.5",
-    genres:
-      [
-        "Self-Help"
-        ,
-        "non-fiction"
-      ],
-    plot: "No matter your goals, Atomic Habits offers a proven framework for improving--every day. James Clear, one of the world's leading experts on habit formation, reveals practical strategies that will teach you exactly how to form good habits, break bad ones, and master the tiny behaviors that lead to remarkable results. If you're having trouble changing your habits, the problem isn't you.",
-    pages: 350,
-    language: "English",
-    dateOfPublish: "10-sep-2017"
-  },
+  booksDemo = [
+    {
+      title: "Atomic Habits",
+      author: "James Clear",
+      imgSrc: "./assets/picture/atomic.jpg",
+      rating: "4.5",
+      genres:
+        [
+          "Self-Help"
+          ,
+          "non-fiction"
+        ],
+      plot: "No matter your goals, Atomic Habits offers a proven framework for improving--every day. James Clear, one of the world's leading experts on habit formation, reveals practical strategies that will teach you exactly how to form good habits, break bad ones, and master the tiny behaviors that lead to remarkable results. If you're having trouble changing your habits, the problem isn't you.",
+      pages: 350,
+      language: "English",
+      dateOfPublish: "10-sep-2017"
+    },
     {
       title: "thE SUBTLE art",
       author: "James Clear",
@@ -216,28 +196,38 @@ export class MyBookInfoComponent implements OnInit {
   right = true
 
 
-  constructor(private bookService: BooInfoService) {
+  constructor(private bookService: BooInfoService, private movedBook: BookDataService) {
   }
 
   ngOnInit(): void {
-
+    this.movedBook.bookData.subscribe(data => {
+      this.currentBookInfo = data
+    })
     // this.similarAuthorService();
-  
   }
 
   onBtnDown() {
     console.log(this.counter)
-    if(this.counter>5)this.counter=0;
-this.counter++;
-   }
+    if (this.counter > 5) this.counter = 0;
+    this.counter++;
+  }
 
-  //  similarAuthorService(){
-  //   this.bookService.getByAuthor(this.currentBookInfo.author)
-  //   .subscribe(res=>{
-  // this.similarAuthorBooks=res;
+  localRate = 0;
 
-  //   })
-  
-  // }
+  setRate(i: number) {
+    this.localRate = i;
+    console.log(i);
+  }
 
+  getGenres(s: string) {
+    let arr = s.replace(/[{}']/gi, "").split(',')
+    return arr.sort()
+  }
+
+  getRate() {
+    if (this.localRate) {
+      console.log("rate lev " + this.localRate)
+      return this.localRate;
+    } else return 0;
+  }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,4 @@ export const environment = {
   production: true,
   apiUrl: 'http://176.29.9.132/python/',
   endPointUrl:'http://176.29.9.132/',
-
 };
