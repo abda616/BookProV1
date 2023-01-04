0ce3517e7e194d48fc4629ab824f6fddff4cfe71
abd_a616 <abdalrhmanalherbawi@gmail.com>
2023-01-04 13:14:29 +0300

    login  and search

--- src/app/search-page/search-page.component.ts
+++ src/app/search-page/search-page.component.ts
@@ -1,142 +1,124 @@
-import {Component, OnInit, EventEmitter, Output, AfterViewInit} from '@angular/core';
+import {Component, OnInit, EventEmitter, Output, AfterViewInit, ViewChild, ViewChildren, ElementRef,} from '@angular/core';
 import {SearchPageService} from '../services/search.service';
 import {SharedServiceService} from '../services/shared-service.service';
-import {searchDataTransferService} from "../services/Transfer/search-data-transfer.service";
-import {SharedDataModule} from "../shared/shared-data.module";
+import {searchDataTransferService} from '../services/Transfer/search-data-transfer.service';
 
 @Component({
   selector: 'app-search-page',
   templateUrl: './search-page.component.html',
-  styleUrls: ['./search-page.component.css']
+  styleUrls: ['./search-page.component.css'],
 })
 export class SearchPageComponent implements OnInit, AfterViewInit {
   numOfColumns = [1, 2, 3, 4, 5];
   filteredSearchResult = [];
-  images = "";
-  targetBook = "romance";
-  searchOption = ['all', 'title', 'author', 'genre', 'description']
-  searchResult = [];
-  isFiltered = false;
-  isGenre: boolean = false;
-  filterGenresArr = [];
-  searchInput: string = '';
-  didRate: boolean = false;
   searchOptions = ['All', 'Title', 'Author', 'Genre', 'Description'];
-  genresFilter = [];
-  stars = [1, 2, 3, 4, 5];
+  searchResult = [];
+  filterGenresArr = [];
+  isFiltered = false;
+  searchInput: string = '';
+  didRate: boolean
+  searchType: string = "all";
+  @ViewChild('searchBar') inputElementRef: ElementRef;
 
-  desiredRating: number = 0;
-  isFiltersClicked: boolean = false;
+
   @Output() changedSearchText: EventEmitter<string> =
     new EventEmitter<string>();
 
-  constructor(private searchService: SearchPageService, private search: searchDataTransferService,
-              private sharedService: SharedServiceService,private sharedData: SharedDataModule) {
+  constructor(
+    private searchService: SearchPageService,
+    private search: searchDataTransferService,
+    private sharedService: SharedServiceService
+  ) {
   }
 
   ngAfterViewInit(): void {
-    setTimeout(() => {this.search.updatePosition(false);}, 0);
+    setTimeout(() => {
+      this.search.updatePosition(false);
+    }, 0);
+    this.inputElementRef.nativeElement.focus();
   }
-
-
 
   ngOnInit(): void {
-    this.search.searchData.subscribe(data => {
+    this.search.searchData.subscribe((data) => {
       this.searchInput = data;
-      this.onSearchAll();
+      this.onSearch(this.searchType);
     });
-    this.sharedData.Interests.subscribe(data=>{
-      this.genresFilter =data;
-    })
   }
 
-  onSearchAll() {
+  onSearch(searchType) {
+
     if (this.searchInput != '') {
-      console.log("on start "+new Date().getSeconds())
-      this.searchService.searchByAll(this.searchInput).subscribe((res) => {
+
+      this.searchService.searchByAll(this.searchInput, this.searchType).subscribe((res) => {
         this.searchResult = res;
-        console.log("set and remove"+new Date().getSeconds())
         this.searchResult = this.sharedService.removeNoImage(this.searchResult);
-        //calling the shared service to change the url to get the large img
+
+        // calling the shared service to change the url to get the large img
         this.searchResult.forEach((e) => {
           e.cover_page = this.sharedService.getLargeImg(
             e.cover_page,
             this.sharedService.getPosition(e.cover_page, 'm/', 2)
           );
         });
-        console.log("end"+new Date().getSeconds())
       });
     } else console.log('empty search');
-
-    return this.searchResult
+    return this.searchResult;
   }
 
   onGetData() {
-    if (this.didRate) {
-      return this.filterRatings();
-    } else if (this.isGenre) {
-      return this.filterGenre(this.filterGenresArr)
-    } else return this.searchResult
+
+    if (this.isFiltered) {
+      return this.filteredSearchResult
+    } else return this.searchResult;
   }
 
   onSearchChange(val) {
-    console.log("1"+new Date().getSeconds())
     this.search.updateData(val);
-    this.onSearchAll();
+    this.onSearch(this.searchType);
   }
 
-  showFilters(element) {
-    let maxHeight;
-    if (this.isFiltersClicked) {
-      this.isFiltersClicked = false;
-      maxHeight = '0px';
-      return maxHeight;
-    } else {
-      this.isFiltersClicked = true;
-      maxHeight = '280px';
-      return maxHeight;
-    }
-  }
 
   getUrl(e) {
     console.log(e.cover_page);
     return e.cover_page;
   }
 
+  onSearchBy(event) {
+    console.log(this.searchResult)
+    this.searchType = event.target.value.toLowerCase()
+    this.onSearch(this.searchType)
+  }
 
   ///fill the stars on click
-  fillStars(targetRating) {
-    let starsElements = document.querySelectorAll('.fa-regular');
-
-    starsElements.forEach((e) => {
-      if (this.didRate) {
-        e.classList.remove("fa-solid")
-        console.log("clicked before")
-      }
-      if (e.id <= targetRating.id) {
-        e.classList.add("fa-solid")
-        this.didRate = true
-        this.desiredRating = targetRating.id
-      }
-    });
-    this.isFiltered = true;
-  }
+  // fillStars(targetRating) {
+  //   let starsElemnts = document.querySelectorAll('.fa-regular');
+  //   starsElemnts.forEach((e) => {
+  //     if (this.filterationData.didRate) {
+  //       e.classList.remove('fa-solid');
+  //       console.log('clicked before');
+  //     }
+  //     if (e.id <= targetRating.id) {
+  //       e.classList.add('fa-solid');
+  //       this.filterationData.didRate = true;
+  //       this.filterationData.desiredRating = targetRating.id;
+  //     }
+  //   });
+  //   this.isFiltered = true;
+  // }
 
   ////filter ratings
-  filterRatings() {
-    return this.searchResult.map(e => {
-      return Math.floor(e.book_average_rating) >= this.desiredRating ? e : "";
-    })
-  }
 
-  filterGenre(targetGenre) {
-    this.filterGenresArr.push(targetGenre);
-    console.log(targetGenre);
-    this.isGenre = true;
-    return this.searchResult.map(e => {
-      return e.genres.forEach(e => {
-        if (e == targetGenre) return e
-      })
-    })
-  }
+  // filterRatings() {
+  //   let temp = [];
+  //   this.searchResult.map((e) => {
+  //     if (
+  //       Math.floor(e.book_average_rating) >= this.filterationData.desiredRating
+  //     ) {
+  //       temp.push(e);
+  //     }
+  //   });
+  //   return temp;
+  // }
+
+
 }
