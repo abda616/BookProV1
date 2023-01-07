10d7b80f3d248446298bf1c32d2d6163165aa209
=armada6199 <moh.abdin27@gmail.com>
2023-01-07 18:54:23 +0300

    with library

--- src/app/my-library/my-library.component.ts
+++ src/app/my-library/my-library.component.ts
@@ -1,6 +1,8 @@
+import { HttpClientModule,HttpClient} from '@angular/common/http';
 import {AfterViewInit, Component, OnInit} from '@angular/core';
 import {searchDataTransferService} from "../services/Transfer/search-data-transfer.service";
-
+import {Book} from "../shared/Interfaces/Book";
+import { environment } from 'src/environments/environment.prod';
 @Component({
   selector: 'app-my-library',
   templateUrl: './my-library.component.html',
@@ -8,9 +10,15 @@ import {searchDataTransferService} from "../services/Transfer/search-data-transf
 
 })
 export class MyLibraryComponent implements OnInit,AfterViewInit {
-  constructor(private search:searchDataTransferService) {
+  data:object;
+ damn=[1,2,3,4,5];
+  constructor(private search:searchDataTransferService,
+    private http:HttpClient,
+
+    ) {
   }
   ngAfterViewInit(): void {
+    this.test()
     setTimeout(()=>{
       this.search.updatePosition(true);
     },0)
@@ -18,46 +26,10 @@ export class MyLibraryComponent implements OnInit,AfterViewInit {
   ngOnInit(): void {
 
   }
-  /*public arr:any[]=[]
-  public firstIteration:any=[];
-  public secondIteration:any=[];
-  public thirdIteration:any=[];
-  public fourthIteration:any=[];
-  public fifthIteration:any=[];
-  public requestTypes={
-    recommendedByAuthor:"recommendByAuthor",
-    recommendedBySimilarBooks:"recommendBySimilarBooks",
-    recommendedByGenre:"recommendByGenre",
-    mostRated:"mostRated"
+  test(){
+    this.http.get<Book>("http://176.29.9.132/python/topn").subscribe(res=>{
+      this.data=res;
+      console.log(this.data)
+    })
   }
-  constructor(private _genureService:MainPageService) {}
-
-  ngOnInit(): void {
-    this._genureService.getSimilar("Twilight")
-      .subscribe(res=>{
-        this.arr=res;
-        this.arr.forEach((ele,index)=>{
-          if(index<2){
-            this.firstIteration.push(ele)
-          }
-          if(index>=2&&index<4){
-            this.secondIteration.push(ele)
-          }
-          if(index>=4&&index<6){
-            this.thirdIteration.push(ele)
-          }
-          if(index>=6&&index<8){
-            this.fourthIteration.push(ele)
-
-          }
-          if(index>=8&&index<10){
-            this.fifthIteration.push(ele)
-          }
-
-        })
-        console.log(this.secondIteration)
-      })
-  }
-  viewRightCard(){
-  }*/
 }
