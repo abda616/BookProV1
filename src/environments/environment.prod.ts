9bb4905671a942a3fa3b0d66ae08592e7975a108
abd_a616 <abdalrhmanalherbawi@gmail.com>
2023-01-07 21:26:39 +0300

    message page set up update env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://176.29.9.132/',
+  apiUrl: 'http://176.28.192.140/',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://176.29.9.132/',
+  apiUrl: 'http://176.28.192.140/',
 };
 
 /*
