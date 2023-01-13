c5b7e48486f9aaf4c7b8d0a4f9ef9e3dc1425386
=armada6199 <moh.abdin27@gmail.com>
2023-01-13 22:30:33 +0300

    adding styles to the book info

--- src/app/my-library/my-library.component.ts
+++ src/app/my-library/my-library.component.ts
@@ -1,9 +1,9 @@
-import {AfterViewInit, Component, OnInit} from '@angular/core';
-import {searchDataTransferService} from "../services/Transfer/search-data-transfer.service";
-import {environment} from 'src/environments/environment.prod';
-import {SharedServiceService} from '../services/shared-service.service';
-import {Book, ownedBooks} from '../shared/Interfaces/Book';
-import {HttpClient} from "@angular/common/http";
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { searchDataTransferService } from '../services/Transfer/search-data-transfer.service';
+import { environment } from 'src/environments/environment.prod';
+import { SharedServiceService } from '../services/shared-service.service';
+import { Book, ownedBooks } from '../shared/Interfaces/Book';
+import { HttpClient } from '@angular/common/http';
 import { MessagesService } from '../services/message/messages.service';
 import { ExchangeService } from '../services/Exchange/exchange.service';
 import { BookDataService } from '../services/Transfer/book-data.service';
@@ -12,116 +12,116 @@ import { BookDataService } from '../services/Transfer/book-data.service';
   templateUrl: './my-library.component.html',
   styleUrls: ['./my-library.component.css'],
 })
-
 export class MyLibraryComponent implements OnInit, AfterViewInit {
-  sectionsArr = ["My Books", "Favorite Books", "Trade List"]
+  sectionsArr = ['My Books', 'Favorite Books', 'Trade List'];
   desiredLibrary: string = this.sectionsArr[0];
   ownedBooks: ownedBooks[];
-  ownedObj = []
+  ownedObj = [];
   favoriteBooks = [];
   favoritesIds = [];
   tradeList = [];
   isAvailable: boolean = false;
 
-  constructor(private search: searchDataTransferService,
-              private http: HttpClient,
-              private sharedService: SharedServiceService,
-              private messageService:MessagesService,
-              private exchageService:ExchangeService,
-              private bookDataService:BookDataService,
-  ) {
-  }
+  constructor(
+    private search: searchDataTransferService,
+    private http: HttpClient,
+    private sharedService: SharedServiceService,
+    private messageService: MessagesService,
+    private exchageService: ExchangeService,
+    private bookDataService: BookDataService
+  ) {}
 
   ngAfterViewInit(): void {
     setTimeout(() => {
       this.search.updatePosition(true);
-    }, 0)
+    }, 0);
   }
 
   ngOnInit(): void {
     this.getOwnedBooks();
     this.getFavoriteBooks();
-
-
   }
-//change the targeted library
+  //change the targeted library
   changeTarget(type) {
     this.desiredLibrary = type;
 
     if (type == this.sectionsArr[0]) {
-      this.getOwnedBooks()
+      this.getOwnedBooks();
     } else if (type == this.sectionsArr[1]) {
-      this.getFavoriteBooks()
+      this.getFavoriteBooks();
     } else if (type == this.sectionsArr[2]) {
-      2
+      2;
       return this.getTradeList();
     }
-    return ''
-
+    return '';
   }
-//get the owned books
+  //get the owned books
   getOwnedBooks() {
-    this.http.get<ownedBooks[]>(environment.apiUrl + "profile/owned").subscribe(res => {
-      this.ownedBooks = res
-      this.ownedBooks = this.sharedService.removeNoImage(this.ownedBooks);
-
-    })
-
+    this.http
+      .get<ownedBooks[]>(environment.apiUrl + 'profile/owned')
+      .subscribe((res) => {
+        this.ownedBooks = res;
+        this.ownedBooks = this.sharedService.removeNoImage(this.ownedBooks);
+      });
   }
-//get favorite books
+  //get favorite books
   getFavoriteBooks() {
-    this.http.get<ownedBooks[]>(environment.apiUrl + "profile/favorites").subscribe(res => {
-      this.favoriteBooks = res;
-      this.favoriteBooks = this.sharedService.removeNoImage(this.favoriteBooks)
-    })
-
+    this.http
+      .get<ownedBooks[]>(environment.apiUrl + 'profile/favorites')
+      .subscribe((res) => {
+        this.favoriteBooks = res;
+        this.favoriteBooks = this.sharedService.removeNoImage(
+          this.favoriteBooks
+        );
+      });
   }
-//get tradeList
+  //get tradeList
   getTradeList(newBook?) {
-   ///check if the trade array is empty at first to avoide duplication 
-   if(this.tradeList.length==0){
-    this.ownedBooks.forEach(e=>{
-      if(e.avaliable) this.tradeList.push(e)//if its available add it to trade list
-    })
-   }else {
-    //if its not empty check weather the passed book is already in the tradelist 
-if(newBook){
-  let bookFound=false;
-    this.tradeList.forEach(e=>{
-      if(e.id==newBook.id) bookFound=true
-    
-    })
-  //if its not found push it to the trade list 
-    if(!bookFound){
-      this.tradeList.push(newBook)
+    ///check if the trade array is empty at first to avoide duplication
+    if (this.tradeList.length == 0) {
+      this.ownedBooks.forEach((e) => {
+        if (e.avaliable) this.tradeList.push(e); //if its available add it to trade list
+      });
+    } else {
+      //if its not empty check weather the passed book is already in the tradelist
+      if (newBook) {
+        let bookFound = false;
+        this.tradeList.forEach((e) => {
+          if (e.id == newBook.id) bookFound = true;
+        });
+        //if its not found push it to the trade list
+        if (!bookFound) {
+          this.tradeList.push(newBook);
+        }
+      }
     }
-}}
 
-    return this.tradeList
+    return this.tradeList;
   }
-  //add the  books to tradeLIST 
+  //add the  books to tradeLIST
   addToTrade(obj, availablity): void {
-  this.bookDataService.tradeThisBook(obj.id,availablity).subscribe(res=>{
-    console.log(res)
-  })
-  //loop over the array and check if the passed object from the html is the same as the obj in the owned array then make it available
-  this.ownedBooks.forEach(e=>{
-    if(e.id==obj.id){
-      e.avaliable=!e.avaliable;
-    }//if its not available remove it from the trade list 
-    if(!e.avaliable){
-      this.removeItemFromTrade(obj)
-    }
-  })
-this.getTradeList(obj);
-
+    this.bookDataService.tradeThisBook(obj.id, availablity).subscribe((res) => {
+      console.log(res);
+    });
+    //loop over the array and check if the passed object from the html is the same as the obj in the owned array then make it available
+    this.ownedBooks.forEach((e) => {
+      if (e.id == obj.id) {
+        e.avaliable = !e.avaliable;
+      } //if its not available remove it from the trade list
+      if (!e.avaliable) {
+        this.removeItemFromTrade(obj);
+      }
+    });
+    this.getTradeList(obj);
   }
   //just a simple function to remove the item using the filter method
-  removeItemFromTrade(item){
-this.tradeList=this.tradeList.filter(e=>{return !item})
+  removeItemFromTrade(item) {
+    this.tradeList = this.tradeList.filter((e) => {
+      return !item;
+    });
   }
 
-//the final function to get the data and the final results to the html 
+  //the final function to get the data and the final results to the html
   getData(type?) {
     if (type == this.sectionsArr[0]) {
       return this.ownedBooks;
@@ -131,8 +131,6 @@ this.tradeList=this.tradeList.filter(e=>{return !item})
     } else if (type == this.sectionsArr[2]) {
       return this.tradeList;
     }
-    return ''
+    return '';
   }
-
-
 }
