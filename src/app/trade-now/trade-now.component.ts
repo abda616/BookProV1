d528f11f1d8121935fde397c86f79555a8c5bfbf
=armada6199 <moh.abdin27@gmail.com>
2023-01-16 18:43:38 +0300

    trade

--- src/app/trade-now/trade-now.component.ts
+++ src/app/trade-now/trade-now.component.ts
@@ -1,4 +1,4 @@
-import {AfterViewInit, Component, OnInit} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {searchDataTransferService} from "../services/Transfer/search-data-transfer.service";
 import {ExchangeService} from "../services/Exchange/exchange.service";
 import {ownedBooks} from '../shared/Interfaces/Book';
@@ -15,8 +15,15 @@ export class TradeNowComponent implements OnInit, AfterViewInit {
   headsInTop: string[] = ["Your Trade List", "Discover Books"];
   yourTradeList = [];
   OtherBooks = [];
-
-
+tradeToggleBtn="Show More"
+exchangeToggleBtn="Show More"
+currentExchangeBook;
+currentBookObj;
+initExchange:boolean=false
+onExchange:boolean=false;
+selectedFromTrade:boolean=false;
+selectedFromOffers:boolean=false;
+@ViewChild('scrollTop') scrollTop:ElementRef;
   constructor(private search: searchDataTransferService,
               private Ex: ExchangeService,
               private moveBook: BookDataService, private router: Router,
@@ -80,8 +87,53 @@ export class TradeNowComponent implements OnInit, AfterViewInit {
   goToDataPage(bookElement: any) {
 
   }
-  onEchangeUi(elementId){
-    console.log(elementId)
+  onExchangeUi(elementId?){
+    let exchangeOffers=document.querySelector(".exchange-offers")
+    if(elementId!="null"){
+      let booksArr=document.querySelectorAll(".book");
+      booksArr.forEach(e=>{
+        if(e.id==elementId){
+  this.currentExchangeBook=e;
+        }
+      })
+      this.yourTradeList.forEach(e=>{
+        if(e.id==elementId){
+        this.currentBookObj=e;
+        }
+      })
+      this.currentExchangeBook.classList.toggle("onExchange")
+  this.onExchange=!this.onExchange
+  exchangeOffers.classList.toggle("show-exchange-offers")
+  if(exchangeOffers.classList.contains("show-exchange-offers")) this.initExchange=true
+    
+}
+  
+    
+  }
+  toggleTrade(contType){
+    
+    if(contType.classList.contains("trade-List-cont")){
+      let tradeListCont=document.querySelector(".trade-List-cont");
+      tradeListCont.classList.toggle("show-more-trade")
+      if(tradeListCont.classList.contains("show-more-trade")){
+        this.tradeToggleBtn="Show Less"
+      } else this.tradeToggleBtn="Show More"
+     
+     
+      console.log(tradeListCont)
+  return this.tradeToggleBtn
+    }else if (contType.classList.contains("exchangable-cont")){
+      let exchangeCont=document.querySelector(".exchangable-cont");
+      exchangeCont.classList.toggle("show-more-exchange")
+      if(exchangeCont.classList.contains("show-more-exchange")){
+        this.exchangeToggleBtn="Show Less"
+      }else this.exchangeToggleBtn="Show More"
+     
+      console.log(exchangeCont)
+  return this.exchangeToggleBtn
+    }
+    return ''
+   
   }
   getData(){
     return this.yourTradeList
